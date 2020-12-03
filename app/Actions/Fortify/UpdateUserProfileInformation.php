14530449e0a7da321d0ae9021b2daac3413c6b8e
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-03 22:34:43 +0100

    Add mobile number validation

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use App\Rules\Mobile;
 use Illuminate\Validation\Rule;
 use App\Contracts\MustVerifyMobile;
 use Illuminate\Support\Facades\Validator;
@@ -21,7 +22,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name'   => ['required', 'string', 'max:255'],
             'email'  => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'mobile' => ['required', 'string', 'max:255', Rule::unique('users')->ignore($user->id)],
+            'mobile' => ['required', 'string', new Mobile, Rule::unique('users')->ignore($user->id)],
             'photo'  => ['nullable', 'image', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
 
