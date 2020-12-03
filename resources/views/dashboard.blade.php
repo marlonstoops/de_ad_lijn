be977a33cfce63a024b60582fe4a2a487f8ee971
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-03 23:00:24 +0100

    Add todos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Welcome to the ADlijn!
         </h2>
     </x-slot>
 
@@ -9,11 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                    <div class="mt-8 text-2xl">
-                        Welcome to your Jetstream application!
-                    </div>
+                    <p>
+                        <!-- TODO -->
+                    </p>
 
                     @if(\Auth::user() && \Auth::user()->hasCredit())
+                        <div class="mt-6 text-gray-500">
+                            <!-- TODO -->
+                            <b>{{ \Auth::user()->credit }}</b>
+                        </div>
                         <div class="mt-6 text-gray-500">
                             <form
                                 method="POST"
@@ -71,6 +75,8 @@
                                 </button>
                             </form>
                         </div>
+                    @else
+                        <!-- TODO -->
                     @endif
 
                 </div>
