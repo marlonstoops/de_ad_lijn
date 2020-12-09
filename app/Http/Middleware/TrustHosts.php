7f0d7fe02347af39a1adff17450ec02391d83c5c
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-09 21:33:23 +0100

    Add interhost to trust hosts

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -14,6 +14,7 @@ class TrustHosts extends Middleware
     public function hosts()
     {
         return [
+            "^(.+\.)?interhostsolutions\.be$",
             $this->allSubdomainsOfApplicationUrl(),
         ];
     }
