559c376dd35251b06e9da57edc871ca94144df73
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-10 22:16:00 +0100

    Add test mail

---
+++ app/Console/Commands/TestMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mail:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send a test mail';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Mail::raw('TEST TEST TEST', function ($message) {
+            $message->to('testing@robvankeilegom.be')
+                ->subject('TEST SUBJECT');
+        });
+    }
+}
