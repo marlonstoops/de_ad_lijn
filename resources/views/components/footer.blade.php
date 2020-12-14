fca381c0c3a9e35202b9859f310443572456f5d2
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-14 20:10:36 +0100

    Fix footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,32 @@
+<footer class="footer bg-white relative pt-1 border-b-2 border-yellow-400">
+    <div class="container mx-auto px-6">
+        <div class="mt-16 flex flex-col items-center">
+            <div class="sm:w-2/3 text-center py-6 text-gray-400 text-sm">
+                Icon made by
+                <a
+                    href="https://www.flaticon.com/authors/good-ware"
+                    title="Good Ware"
+                >Good Ware</a>
+                from
+                <a
+                    href="https://www.flaticon.com/"
+                    title="Flaticon"
+                >
+                    www.flaticon.com
+                </a>
+            </div>
+            <div class="sm:w-2/3 text-center py-6">
+                <p class="text-sm text-gray-600 font-bold mb-2">
+                    © 2020 by Marlon Stoops &
+                    <a
+                        href="https://robvankeilegom.be"
+                        title="robvankeilegom.be"
+                    >
+                        Rob Van Keilegom
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+</footer>
