efac088255d5a303c2f5e1b7564153c1c2708451
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-14 15:33:45 +0100

    Force https in production env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
