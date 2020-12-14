600abc0b9151991b847bc500f118d0805d8cdab1
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-14 21:11:13 +0100

    Update spacing on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,73 +14,72 @@
                     </p>
 
                     @if(\Auth::user() && \Auth::user()->hasCredit())
-                        <div class="mt-6 text-gray-500">
-                            <!-- TODO -->
-                            <b>{{ \Auth::user()->credit }}</b>
-                        </div>
-                        <div class="mt-6 text-gray-500">
-                            <form
-                                method="POST"
-                                action="{{ route('ad-lijn') }}"
-                            >
-                                @csrf
-
-                                <!-- Name -->
-                                <div class="col-span-6 sm:col-span-4">
-                                    <x-jet-label
-                                        for="name"
-                                        value="{{ __('Name') }}"
-                                    />
-                                    <x-jet-input
-                                        id="name"
-                                        name="name"
-                                        type="text"
-                                        class="mt-1 block w-full"
-                                        value="{{ old('name') }}"
-                                        wire:model.defer="state.name"
-                                        autocomplete="name"
-                                    />
-                                    <x-jet-input-error
-                                        for="name"
-                                        class="mt-2"
-                                    />
-                                </div>
-
-                                <!-- Mobile -->
-                                <div class="col-span-6 sm:col-span-4">
-                                    <x-jet-label
-                                        for="mobile"
-                                        value="{{ __('Mobile') }}"
-                                    />
-                                    <x-jet-input
-                                        id="mobile"
-                                        name="mobile"
-                                        type="text"
-                                        class="mt-1 block w-full"
-                                        value="{{ old('mobile') }}"
-                                        wire:model.defer="state.mobile"
-                                    />
-                                    <x-jet-input-error
-                                        for="mobile"
-                                        class="mt-2"
-                                    />
-                                </div>
-
-                                <button
-                                    type="submit"
-                                    class="mt-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150"
-                                >
-
-                                    {{ __('Save') }}
-                                </button>
-                            </form>
-                        </div>
-                    @else
+                    <div class="mt-6 text-gray-500">
                         <!-- TODO -->
+                        <b>{{ \Auth::user()->credit }}</b>
+                    </div>
+                    <div class="mt-6 text-gray-500">
+                        <form
+                            method="POST"
+                            action="{{ route('ad-lijn') }}"
+                        >
+                            @csrf
+
+                            <!-- Name -->
+                            <div class="col-span-7 sm:col-span-4 mb-6">
+                                <x-jet-label
+                                    for="name"
+                                    value="{{ __('Name') }}"
+                                />
+                                <x-jet-input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    class="mt-1 block w-full"
+                                    value="{{ old('name') }}"
+                                    wire:model.defer="state.name"
+                                    autocomplete="name"
+                                />
+                                <x-jet-input-error
+                                    for="name"
+                                    class="mt-2"
+                                />
+                            </div>
+
+                            <!-- Mobile -->
+                            <div class="col-span-6 sm:col-span-4 mb-6">
+                                <x-jet-label
+                                    for="mobile"
+                                    value="{{ __('Mobile') }}"
+                                />
+                                <x-jet-input
+                                    id="mobile"
+                                    name="mobile"
+                                    type="text"
+                                    class="mt-1 block w-full"
+                                    value="{{ old('mobile') }}"
+                                    wire:model.defer="state.mobile"
+                                />
+                                <x-jet-input-error
+                                    for="mobile"
+                                    class="mt-2"
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                class="mt-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150"
+                            >
+
+                                {{ __('Call User!') }}
+                            </button>
+                        </form>
+                    </div>
+                    @else
+                    <!-- TODO -->
                     @endif
 
                 </div>
             </div>
         </div>
 </x-app-layout>
-
