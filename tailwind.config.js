c2bc2a2f990c84a156715d2d2c0cd94ffe73b788
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-14 18:49:27 +0100

    Update to tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require( 'tailwindcss/defaultTheme' );
 
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  purge: [
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [ 'Nunito', ...defaultTheme.fontFamily.sans ],
+      },
     },
+  },
 
-    variants: {
-        opacity: ['responsive', 'hover', 'focus', 'disabled'],
-    },
+  variants: {
+    opacity: [ 'responsive', 'hover', 'focus', 'disabled' ],
+  },
 
-    plugins: [require('@tailwindcss/ui')],
 };
+
