f499a583cb718af3c411c2fe4395b07d4244cb49
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-21 19:11:42 +0100

    Format file

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,36 +6,94 @@
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register') }}">
+        <form
+            method="POST"
+            action="{{ route('register') }}"
+        >
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-label
+                    for="name"
+                    value="{{ __('Name') }}"
+                />
+                <x-jet-input
+                    id="name"
+                    class="block mt-1 w-full"
+                    type="text"
+                    name="name"
+                    :value="old('name')"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-label
+                    for="email"
+                    value="{{ __('Email') }}"
+                />
+                <x-jet-input
+                    id="email"
+                    class="block mt-1 w-full"
+                    type="email"
+                    name="email"
+                    :value="old('email')"
+                    required
+                />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="mobile" value="{{ __('Mobile') }}" />
-                <x-jet-input id="mobile" class="block mt-1 w-full" type="text" name="mobile" :value="old('mobile')" required />
+                <x-jet-label
+                    for="mobile"
+                    value="{{ __('Mobile') }}"
+                />
+                <x-jet-input
+                    id="mobile"
+                    class="block mt-1 w-full"
+                    type="text"
+                    name="mobile"
+                    :value="old('mobile')"
+                    required
+                />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-label
+                    for="password"
+                    value="{{ __('Password') }}"
+                />
+                <x-jet-input
+                    id="password"
+                    class="block mt-1 w-full"
+                    type="password"
+                    name="password"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-jet-label
+                    for="password_confirmation"
+                    value="{{ __('Confirm Password') }}"
+                />
+                <x-jet-input
+                    id="password_confirmation"
+                    class="block mt-1 w-full"
+                    type="password"
+                    name="password_confirmation"
+                    required
+                    autocomplete="new-password"
+                />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a
+                    class="underline text-sm text-gray-600 hover:text-gray-900"
+                    href="{{ route('login') }}"
+                >
                     {{ __('Already registered?') }}
                 </a>
 
