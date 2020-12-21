a1eb551d2dc946bdeec45aff123d06ea03e75022
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-21 19:18:14 +0100

    Add terms url to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,22 +1,30 @@
 <footer class="footer bg-white relative pt-1 border-b-2 border-yellow-400">
     <div class="container mx-auto px-6">
-        <div class="mt-16 flex flex-col items-center">
-            <div class="sm:w-2/3 text-center py-6 text-gray-400 text-sm">
+        <div class="mt-16 flex flex-col items-center text-sm">
+            <div class="sm:w-2/3 text-center py-6 text-gray-400">
                 Icon made by
                 <a
+                    class="hover:text-gray-900"
                     href="https://www.flaticon.com/authors/good-ware"
                     title="Good Ware"
                 >Good Ware</a>
                 from
                 <a
+                    class="hover:text-gray-900"
                     href="https://www.flaticon.com/"
                     title="Flaticon"
                 >
                     www.flaticon.com
                 </a>
             </div>
+            <a
+                href="{{ route('terms') }}"
+                class="mt-2 text-gray-400 hover:text-gray-900"
+            >
+                {{ __('Terms and Conditions') }}
+            </a>
             <div class="sm:w-2/3 text-center py-6">
-                <p class="text-sm text-gray-600 font-bold mb-2">
+                <p class="text-gray-600 font-bold mb-2">
                     © 2020 by Marlon Stoops &
                     <a
                         href="https://robvankeilegom.be"
