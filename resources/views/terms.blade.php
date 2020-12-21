d12a8db9bd1fb812d4e34de3c75f6d3f5c32012d
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-21 19:09:40 +0100

    Change page to use tailwind classes

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,122 +1,144 @@
 <x-guest-layout>
-@extends('layouts.app')
+    <div class="m-auto px-4 py-8 max-w-xl">
 
-@section('content')
-<div class="columns section title-section">
-    <div class="column is-8">
-        <div class="title">Terms of Service</div>
+
+
+        <div class="px-4 py-2 mt-2 bg-white">
+            <h2 class="font-bold text-2xl text-gray-800">
+                Terms Of Service
+            </h2>
+
+
+            <div class="sm:text-sm text-xs text-gray-700 px-2 mr-1 my-3">
+
+                <h3>1. Terms</h3>
+                <p>By accessing the website at <a href="{{ URL::to('/') }}">{{ URL::to('/') }}</a>, you are agreeing to
+                    be bound by
+                    these terms of service, all applicable laws and regulations, and agree that you are responsible for
+                    compliance
+                    with any applicable local laws. If you do not agree with any of these terms, you are prohibited from
+                    using or
+                    accessing this site. The materials contained in this website are protected by applicable copyright
+                    and trademark
+                    law.</p>
+                <h3>2. Use License</h3>
+                <ol type="a">
+                    <li>Permission is granted to temporarily download one copy of the materials (information or
+                        software) on
+                        {{ config('app.name') }}'s website for personal, non-commercial transitory viewing only. This is
+                        the grant
+                        of a license, not a transfer of title, and under this license you may not:
+                        <ol type="i">
+                            <li>modify or copy the materials;</li>
+                            <li>use the materials for any commercial purpose, or for any public display (commercial or
+                                non-commercial);</li>
+                            <li>attempt to decompile or reverse engineer any software contained on
+                                {{ config('app.name') }}'s
+                                website;</li>
+                            <li>remove any copyright or other proprietary notations from the materials; or</li>
+                            <li>transfer the materials to another person or "mirror" the materials on any other server.
+                            </li>
+                        </ol>
+                    </li>
+                    <li>This license shall automatically terminate if you violate any of these restrictions and may be
+                        terminated by
+                        {{ config('app.name') }} at any time. Upon terminating your viewing of these materials or upon
+                        the
+                        termination of this license, you must destroy any downloaded materials in your possession
+                        whether in
+                        electronic or printed format.</li>
+                </ol>
+                <h3>3. Disclaimer</h3>
+                <ol type="a">
+                    <li>The materials on {{ config('app.name') }}'s website are provided on an 'as is' basis.
+                        {{ config('app.name') }} makes no warranties, expressed or implied, and hereby disclaims and
+                        negates all
+                        other warranties including, without limitation, implied warranties or conditions of
+                        merchantability, fitness
+                        for a particular purpose, or non-infringement of intellectual property or other violation of
+                        rights.</li>
+                    <li>Further, {{ config('app.name') }} does not warrant or make any representations concerning the
+                        accuracy,
+                        likely results, or reliability of the use of the materials on its website or otherwise relating
+                        to such
+                        materials or on any sites linked to this site.</li>
+                </ol>
+                <h3>4. Limitations</h3>
+                <p>In no event shall {{ config('app.name') }} or its suppliers be liable for any damages (including,
+                    without
+                    limitation, damages for loss of data or profit, or due to business interruption) arising out of the
+                    use or
+                    inability to use the materials on {{ config('app.name') }}'s website, even if
+                    {{ config('app.name') }} or a
+                    {{ config('app.name') }} authorized representative has been notified orally or in writing of the
+                    possibility of
+                    such damage. Because some jurisdictions do not allow limitations on implied warranties, or
+                    limitations of
+                    liability for consequential or incidental damages, these limitations may not apply to you.</p>
+                <h3>5. Accuracy of materials</h3>
+                <p>The materials appearing on {{ config('app.name') }} website could include technical, typographical,
+                    or
+                    photographic errors. {{ config('app.name') }} does not warrant that any of the materials on its
+                    website are
+                    accurate, complete or current. {{ config('app.name') }} may make changes to the materials contained
+                    on its
+                    website at any time without notice. However {{ config('app.name') }} does not make any commitment to
+                    update the
+                    materials.</p>
+                <h3>6. Links</h3>
+                <p>{{ config('app.name') }} has not reviewed all of the sites linked to its website and is not
+                    responsible for the
+                    contents of any such linked site. The inclusion of any link does not imply endorsement by
+                    {{ config('app.name') }} of the site. Use of any such linked website is at the user's own risk.</p>
+                <h3>7. Modifications</h3>
+                <p>{{ config('app.name') }} may revise these terms of service for its website at any time without
+                    notice. By using
+                    this website you are agreeing to be bound by the then current version of these terms of service.</p>
+                <h3>8. Governing Law</h3>
+                <p>These terms and conditions are governed by and construed in accordance with the laws of Belgium and
+                    you
+                    irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+            </div>
+
+            <h2 class="font-bold text-2xl text-gray-800">
+                Privacy Policy
+            </h2>
+            <div class="sm:text-sm text-xs text-gray-700 px-2 mr-1 my-3">
+                <p>
+                    The privacy of all visitors and members of {{ config('app.name') }} is taken very seriously. All
+                    personal information is strictly confidential and data is used only to provide our services as
+                    quickly and smoothly as possible. Personal information will never be sold to third parties and are
+                    only made available to third parties if these are involved in implementing the services of
+                    {{ config('app.name') }}.
+                </p>
+                <p>
+                    {{ config('app.name') }} uses personal information to deliver the following services:
+                </p>
+                <p>
+                    If you open an {{ config('app.name') }} account, we save your data on a secure server. In your
+                    account we store information such as your name, phone number and email address, so you do not need
+                    any new order form. {{ config('app.name') }} does not sell personal information to third parties.
+                </p>
+                <p>
+                    If we transfer or receive information, we always use encryption technologies that are recognized as
+                    common standards within the IT sector. Both our employees and third parties will at all times deal
+                    with personal data confidentially.
+                </p>
+                <p>
+                    <b>View or change personal data</b> As an {{ config('app.name') }} user you have the right to access
+                    your information to view or modify. Of course you can login to the website and look at or change
+                    your data.
+                </p>
+                <p>
+                    <b>Cookies</b> Cookies are small data files that your browser stores on your computer.
+                    {{ config('app.name') }} uses cookies for the following purposes:
+                </p>
+                <p>
+                    When you log into the {{ config('app.name') }} website a cookie is used to make sure that anywhere
+                    on the site you remain logged in at {{ config('app.name') }}. This allows you to easily navigate the
+                    {{ config('app.name') }} platform.
+                </p>
+            </div>
+        </div>
     </div>
-</div>
-<div class="content">
-    <h3>1. Terms</h3>
-    <p>By accessing the website at <a href="{{ URL::to('/') }}">{{ URL::to('/') }}</a>, you are agreeing to be bound by
-        these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance
-        with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or
-        accessing this site. The materials contained in this website are protected by applicable copyright and trademark
-        law.</p>
-    <h3>2. Use License</h3>
-    <ol type="a">
-        <li>Permission is granted to temporarily download one copy of the materials (information or software) on
-            {{ config('app.name') }}'s website for personal, non-commercial transitory viewing only. This is the grant
-            of a license, not a transfer of title, and under this license you may not:
-            <ol type="i">
-                <li>modify or copy the materials;</li>
-                <li>use the materials for any commercial purpose, or for any public display (commercial or
-                    non-commercial);</li>
-                <li>attempt to decompile or reverse engineer any software contained on {{ config('app.name') }}'s
-                    website;</li>
-                <li>remove any copyright or other proprietary notations from the materials; or</li>
-                <li>transfer the materials to another person or "mirror" the materials on any other server.</li>
-            </ol>
-        </li>
-        <li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by
-            {{ config('app.name') }} at any time. Upon terminating your viewing of these materials or upon the
-            termination of this license, you must destroy any downloaded materials in your possession whether in
-            electronic or printed format.</li>
-    </ol>
-    <h3>3. Disclaimer</h3>
-    <ol type="a">
-        <li>The materials on {{ config('app.name') }}'s website are provided on an 'as is' basis.
-            {{ config('app.name') }} makes no warranties, expressed or implied, and hereby disclaims and negates all
-            other warranties including, without limitation, implied warranties or conditions of merchantability, fitness
-            for a particular purpose, or non-infringement of intellectual property or other violation of rights.</li>
-        <li>Further, {{ config('app.name') }} does not warrant or make any representations concerning the accuracy,
-            likely results, or reliability of the use of the materials on its website or otherwise relating to such
-            materials or on any sites linked to this site.</li>
-    </ol>
-    <h3>4. Limitations</h3>
-    <p>In no event shall {{ config('app.name') }} or its suppliers be liable for any damages (including, without
-        limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or
-        inability to use the materials on {{ config('app.name') }}'s website, even if {{ config('app.name') }} or a
-        {{ config('app.name') }} authorized representative has been notified orally or in writing of the possibility of
-        such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of
-        liability for consequential or incidental damages, these limitations may not apply to you.</p>
-    <h3>5. Accuracy of materials</h3>
-    <p>The materials appearing on {{ config('app.name') }} website could include technical, typographical, or
-        photographic errors. {{ config('app.name') }} does not warrant that any of the materials on its website are
-        accurate, complete or current. {{ config('app.name') }} may make changes to the materials contained on its
-        website at any time without notice. However {{ config('app.name') }} does not make any commitment to update the
-        materials.</p>
-    <h3>6. Links</h3>
-    <p>{{ config('app.name') }} has not reviewed all of the sites linked to its website and is not responsible for the
-        contents of any such linked site. The inclusion of any link does not imply endorsement by
-        {{ config('app.name') }} of the site. Use of any such linked website is at the user's own risk.</p>
-    <h3>7. Modifications</h3>
-    <p>{{ config('app.name') }} may revise these terms of service for its website at any time without notice. By using
-        this website you are agreeing to be bound by the then current version of these terms of service.</p>
-    <h3>8. Governing Law</h3>
-    <p>These terms and conditions are governed by and construed in accordance with the laws of Belgium and you
-        irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
-</div>
-
-<div class="section">
-</div>
-@endsection
-@extends('layouts.app')
-
-@section('content')
-<div class="columns section title-section">
-    <div class="column is-8">
-        <div class="title">Privacy Policy</div>
-    </div>
-</div>
-<div class="content">
-    <p>
-        The privacy of all visitors and members of {{ config('app.name') }} is taken very seriously.
-        All personal information is strictly confidential and data is used only to provide our services as quickly and
-        smoothly as possible.
-        Personal information will never be sold to third parties and are only made available to third parties if these
-        are involved in implementing the services of {{ config('app.name') }}.
-    </p>
-    <p>
-        {{ config('app.name') }} uses personal information to deliver the following services:
-    </p>
-    <p>
-        If you open an {{ config('app.name') }} account, we save your data on a secure server.
-        In your account we store information such as your name, phone number and email address, so you do not need any new order form.
-        {{ config('app.name') }} does not sell personal information to third parties.
-    </p>
-    <p>
-        If we transfer or receive information, we always use encryption technologies that are recognized as common
-        standards within the IT sector.
-        Both our employees and third parties will at all times deal with personal data confidentially.
-    </p>
-    <p>
-        <b>View or change personal data</b>
-        As an {{ config('app.name') }} user you have the right to access your information to view or modify. Of course
-        you can login to the website and look at or change your data.
-    </p>
-    <p>
-        <b>Cookies</b>
-        Cookies are small data files that your browser stores on your computer. {{ config('app.name') }} uses cookies
-        for the following purposes:
-
-    </p>
-    <p>
-        When you log into the {{ config('app.name') }} website a cookie is used to make sure that anywhere on the site
-        you remain logged in at {{ config('app.name') }}.
-        This allows you to easily navigate the {{ config('app.name') }} platform.
-    </p>
-</div>
-@endsection
 </x-guest-layout>
