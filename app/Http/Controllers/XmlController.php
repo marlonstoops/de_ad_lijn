c5292fdc002c38cd95660e19b6495d1872aad6d7
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-12-29 15:06:05 +0100

    Add callback route

--- app/Http/Controllers/XmlController.php
+++ app/Http/Controllers/XmlController.php
@@ -21,4 +21,25 @@ class XmlController extends Controller
             'Content-Type' => 'application/xml',
         ]);
     }
+
+    public function callback()
+    {
+        $xml = '
+            <Response>
+                <Say voice="alice">
+                    Thanks for the call. Configure your number\'s voice U R L to change this message.
+                </Say>
+
+                <Pause length="1"/>
+
+                <Say voice="alice">
+                    Let us know if we can help you in any way during your development.
+                </Say>
+            </Response>
+        ';
+
+        return response($xml, 200, [
+            'Content-Type' => 'application/xml',
+        ]);
+    }
 }
